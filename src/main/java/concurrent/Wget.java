7b8e2566c6459a07c02ae8f7f6c8ca549abe27a9
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-16 15:17:28 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread timer = new Thread(() -> {
+            for (int index = 0; index < 101; index++) {
+                System.out.print("\rLoading : " + index + "%");
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.print("\rLoading finished");
+        });
+        timer.start();
+    }
+}
