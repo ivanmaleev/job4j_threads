48cd2de8c881b53510e48385f63c2fdab3da500a
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-16 15:44:17 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,41 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        int count = 0;
+        String simb = "";
+        while (!Thread.currentThread().isInterrupted()) {
+            switch (count) {
+                case 0:
+                    simb = "|";
+                    break;
+                case 1:
+                    simb = "/";
+                    break;
+                case 2:
+                    simb = "-";
+                    break;
+                case 3:
+                    simb = "\\";
+                    break;
+            }
+            System.out.print("\r loading ... " + simb);
+            try {
+                Thread.sleep(250); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            count++;
+            count = count % 4;
+        }
+        progress.interrupt();
+    }
+
+    public static void main(String[] args) {
+        ConsoleProgress con = new ConsoleProgress();
+        con.run();
+    }
+}
