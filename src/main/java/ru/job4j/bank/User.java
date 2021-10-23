6860b2e5dfbc1755c273883306e1667253c73d9b
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-23 10:38:32 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #219612]

--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -1,4 +1,4 @@
-package ru.job4j.Bank;
+package ru.job4j.bank;
 
 import java.util.Objects;
 
@@ -42,9 +42,9 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", amount=" + amount +
-                '}';
+        return "User{"
+                + "id=" + id
+                + ", amount=" + amount
+                + '}';
     }
 }
--- src/main/java/ru/job4j/bank/UserStore.java
+++ src/main/java/ru/job4j/bank/UserStore.java
@@ -1,4 +1,4 @@
-package ru.job4j.Bank;
+package ru.job4j.bank;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
