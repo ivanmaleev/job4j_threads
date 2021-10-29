bb8cdb56b53de2f607469100dbbb9ba1bf1e0f47
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-10-29 19:27:43 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,25 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(), (k, v) -> {
+            if (model.getVersion() != memory.get(k).getVersion()) {
+                throw new OptimisticException("Not updated");
+            }
+            return model;
+        }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.cache;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CacheTest {
+
+    @Test
+    public void whenAdd() {
+        Base base1 = new Base(1, 1);
+        base1.setName("Base 1");
+        Base base2 = new Base(2, 1);
+        base2.setName("Base 2");
+        Cache cache = new Cache();
+        Assert.assertTrue(cache.add(base1));
+        Assert.assertTrue(cache.add(base2));
+        Assert.assertFalse(cache.add(base2));
+    }
+
+    @Test
+    public void whenUpdate() {
+        Base base1 = new Base(1, 1);
+        base1.setName("Base 1");
+        Cache cache = new Cache();
+        cache.add(base1);
+        base1.setName("Base 111");
+        Assert.assertTrue(cache.update(base1));
+    }
+
+    @Test(expected = OptimisticException.class)
+    public void whenUpdateFalse() {
+        Base base1 = new Base(1, 1);
+        base1.setName("Base 1");
+        Cache cache = new Cache();
+        cache.add(base1);
+        base1 = new Base(1, 2);
+        base1.setName("Base 1");
+        cache.update(base1);
+    }
+
+    @Test
+    public void whenDelete() {
+        Base base1 = new Base(1, 1);
+        base1.setName("Base 1");
+        Cache cache = new Cache();
+        Assert.assertTrue(cache.add(base1));
+        cache.delete(base1);
+        Assert.assertTrue(cache.add(base1));
+    }
+}
